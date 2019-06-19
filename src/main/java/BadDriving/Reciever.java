51221c8de3683be34694739531547110a87291e2
Merry Cimakasky <mkcimakasky@gmail.com>
2019-06-19 10:40:33 -0700

    docs

--- src/main/java/BadDriving/Reciever.java
+++ src/main/java/BadDriving/Reciever.java
@@ -1,15 +1,26 @@
 package BadDriving;
-import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
 
 import javax.annotation.Nullable;
 
 
 public class Reciever {
+
+    /**
+     * variable used to represent firebasefirestore database
+     */
     Firestore db;
+
+    /**
+     * variable used to represent the stringified JSON Object coming from the android controller telling the car which direction to go
+     */
     String direction;
 
-public void getDB() {
+    /**
+     * the method on Receiver which connects with firebase, accesses the firestore database and finds the correct Project, locates the necessary collection and document, and listens on that document for changes.
+     * When changes are heard, it gets the new value of direction off the document car in firebase, and sets that to the String direction in this app
+     */
+    public void getDB() {
     FirestoreOptions firestoreOptions = FirestoreOptions.getDefaultInstance().toBuilder().setProjectId("a-car-you-can-only-drive-badly").build();
     db = firestoreOptions.getService();
     db.collection("driving").document("car").addSnapshotListener(new EventListener<DocumentSnapshot>() {
