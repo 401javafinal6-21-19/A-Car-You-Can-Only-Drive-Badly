fb2c118e824ae261eba19aa6be0e8e87cc6cd192
Merry Cimakasky <mkcimakasky@gmail.com>
2019-06-19 14:14:30 -0700

    moving things around

--- src/main/java/BadDriving/Car.java
+++ src/main/java/BadDriving/Car.java
@@ -1,5 +1,6 @@
 package BadDriving;
 
+import com.google.cloud.firestore.*;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
@@ -7,196 +8,52 @@ import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.*;
 
+import javax.annotation.Nullable;
+
 
 public class Car {
-    final private GpioController gpio;
-    final protected GpioPinDigitalOutput thrustPin26;
-    final protected GpioPinDigitalOutput thrustPin27;
-    final protected GpioPinDigitalOutput thrustPin28;
-    final protected GpioPinDigitalOutput thrustPin29;
 
 
-    public Car() {
-        try {
-            System.out.println("initializing Car object");
+    /**
+     * variable used to represent firebasefirestore database
+     */
+    Firestore db;
 
-            gpio = GpioFactory.getInstance();
-            System.out.println("gpio instance at 24 in car" + gpio);
-            thrustPin26 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26, "thrustPin26", PinState.LOW);
-            thrustPin27 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27, "thrustPin27", PinState.LOW);
-            thrustPin28 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28, "thrustPin28", PinState.LOW);
-            thrustPin29 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29, "thrustPin29", PinState.LOW);
-            System.out.println("pin 26, " + thrustPin26 + " pin 27, " + thrustPin27 + " pin 28, " + thrustPin28+ " pin 29, " + thrustPin29);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Car was not initialized");
-        }
-    }
+    /**
+     * variable used to represent the stringified JSON Object coming from the android controller telling the car which direction to go
+     */
+    String direction;
+
+    /**
+     * the method on Receiver which connects with firebase, accesses the firestore database and finds the correct Project, locates the necessary collection and document, and listens on that document for changes.
+     * When changes are heard, it gets the new value of direction off the document car in firebase, and sets that to the String direction in this app.
+     */
+    public void getDB() {
+        //get instance of firebase firestore database from the project a-car-you-can-only-drive-badly
+        FirestoreOptions firestoreOptions = FirestoreOptions.getDefaultInstance().toBuilder().setProjectId("a-car-you-can-only-drive-badly").build();
+        System.out.println("firestoreOptions on line 26 of the Reciever" + firestoreOptions);
+        //set the db to the instance from the previous line
+        db = firestoreOptions.getService();
+        System.out.println("firestore database on line 29 of the Reciever" + db);
+        //create a listener on the car document in the driving collection in the firebase firestore a-car-you-can-only-drive-badly
+        db.collection("driving").document("car").addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            //on a change in that document...
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirestoreException error) {
+                //...set the json object direction to a string and assign it to the direction variable in this app
+                direction = value.get("direction").toString();
+                System.out.println("Value in Reciever at 37" + value.get("direction"));
 
 
-    public void steeringButtonDepressed() throws InterruptedException {
-        Reciever r = new Reciever();
-        try {
-            switch (r.direction) {
-                case "forward":
-                    forward();
-                    System.out.println("direction in case forward " + r.direction);
-                    break;
+                Steering steering = new Steering();
 
-                case "stop":
-                    stop();
-                    System.out.println("direction in case stop " + r.direction);
-                    break;
 
-                case "left":
-                    left();
-                    System.out.println("direction in case left " + r.direction);
-                    break;
+                steering.steeringButtonDepressed();
+                steering.steeringButtonReleased();
 
-                case "right":
-                    right();
-                    System.out.println("direction in case right " + r.direction);
-                    break;
-
-                default:
-                    stop();
-                    System.out.println("direction in case stop " + r.direction);
-                    break;
 
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Directional methods are not working");
-        }
+        });
     }
-
-    public void steeringButtonReleased() throws InterruptedException {
-        Reciever r = new Reciever();
-        switch (r.direction){
-            case "forward release":
-                forwardRelease();
-                break;
-
-            case "left release":
-                leftRelease();
-                break;
-
-            case "right release":
-                rightRelease();
-                break;
-
-            default: stop();
-                break;
-        }
-    }
-
-//  Everything below this line is within steeringButtonDepressed
-
-    public void forward() throws InterruptedException {
-
-        GpioPinDigitalOutput wheelOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26);
-        GpioPinDigitalOutput wheelTwo = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
-        GpioPinDigitalOutput wheelThree = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
-        GpioPinDigitalOutput wheelFour = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
-
-        wheelOne.high();
-        wheelTwo.high();
-        wheelThree.high();
-        wheelFour.high();
-    }
-
-    public void stop() throws InterruptedException {
-
-        GpioPinDigitalOutput wheelOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26);
-        GpioPinDigitalOutput wheelTwo = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
-        GpioPinDigitalOutput wheelThree = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
-        GpioPinDigitalOutput wheelFour = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
-
-        wheelOne.high();
-        wheelOne.toggle();
-        wheelTwo.high();
-        wheelTwo.toggle();
-        wheelThree.high();
-        wheelThree.toggle();
-        wheelFour.high();
-        wheelFour.toggle();
-    }
-
-    public void left() throws InterruptedException {
-
-        GpioPinDigitalOutput wheelOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26);
-        GpioPinDigitalOutput wheelTwo = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
-        GpioPinDigitalOutput wheelThree = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
-        GpioPinDigitalOutput wheelFour = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
-
-        wheelOne.high();
-        wheelOne.toggle();
-        wheelTwo.high();
-        wheelTwo.toggle();
-        wheelThree.high();
-        wheelFour.high();
-    }
-
-    public void right() throws InterruptedException {
-
-        GpioPinDigitalOutput wheelOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26);
-        GpioPinDigitalOutput wheelTwo = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
-        GpioPinDigitalOutput wheelThree = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
-        GpioPinDigitalOutput wheelFour = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
-
-        wheelOne.high();
-        wheelTwo.high();
-        wheelThree.high();
-        wheelThree.toggle();
-        wheelFour.high();
-        wheelFour.toggle();
-    }
-//  Everything above this line is within steeringButtonDepressed
-
-//  Everything below this line is within steeringButtonReleased
-
-    public void forwardRelease() throws InterruptedException {
-
-        GpioPinDigitalOutput wheelOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26);
-        GpioPinDigitalOutput wheelTwo = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
-        GpioPinDigitalOutput wheelThree = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
-        GpioPinDigitalOutput wheelFour = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
-
-        wheelOne.low();
-        wheelTwo.low();
-        wheelThree.low();
-        wheelFour.low();
-    }
-
-    public void rightRelease() throws InterruptedException {
-
-        GpioPinDigitalOutput wheelOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26);
-        GpioPinDigitalOutput wheelTwo = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
-        GpioPinDigitalOutput wheelThree = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
-        GpioPinDigitalOutput wheelFour = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
-
-        wheelOne.low();
-        wheelTwo.low();
-        wheelThree.low();
-        wheelFour.low();
-    }
-
-    public void leftRelease() throws InterruptedException {
-
-        GpioPinDigitalOutput wheelOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26);
-        GpioPinDigitalOutput wheelTwo = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
-        GpioPinDigitalOutput wheelThree = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
-        GpioPinDigitalOutput wheelFour = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
-
-        wheelOne.high();
-        wheelOne.toggle();
-        wheelTwo.high();
-        wheelTwo.toggle();
-        wheelThree.high();
-        wheelFour.high();
-    }
-
-    //  Everything above this line is within steeringButtonReleased
-
-
 }
+
---
+++ src/main/java/BadDriving/Steering.java
@@ -0,0 +1,228 @@
+package BadDriving;
+
+import com.pi4j.io.gpio.*;
+
+public class Steering {
+
+    final  GpioController gpio;
+    final  GpioPinDigitalOutput thrustPin26;
+    final  GpioPinDigitalOutput thrustPin27;
+    final  GpioPinDigitalOutput thrustPin28;
+    final  GpioPinDigitalOutput thrustPin29;
+
+    public Steering() {
+
+
+
+        try {
+            System.out.println("initializing Car object");
+
+            gpio = GpioFactory.getInstance();
+            System.out.println("gpio instance at 24 in car" + gpio);
+            thrustPin26 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26, "thrustPin26", PinState.LOW);
+            thrustPin27 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27, "thrustPin27", PinState.LOW);
+            thrustPin28 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28, "thrustPin28", PinState.LOW);
+            thrustPin29 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29, "thrustPin29", PinState.LOW);
+            System.out.println("pin 26, " + thrustPin26 + " pin 27, " + thrustPin27 + " pin 28, " + thrustPin28+ " pin 29, " + thrustPin29);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Car was not initialized");
+        }
+    }
+    public void steeringButtonDepressed(){
+        Reciever r = new Reciever();
+        switch (r.direction) {
+            case "forward":
+                try {
+                   forward();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("direction in case forward " + r.direction);
+                break;
+
+            case "stop":
+                try {
+                    stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("direction in case stop " + r.direction);
+                break;
+
+            case "left":
+                try {
+                    left();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("direction in case left " + r.direction);
+                break;
+
+            case "right":
+                try {
+                  right();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("direction in case right " + r.direction);
+                break;
+
+            default:
+                try {
+                   stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("direction in case stop " + r.direction);
+                break;
+
+        }
+
+    }
+
+    public void steeringButtonReleased(){
+        Reciever r = new Reciever();
+        switch (r.direction){
+            case "forward release":
+                try {
+                  forwardRelease();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case "left release":
+                try {
+                   leftRelease();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case "right release":
+                try {
+                    rightRelease();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            default:
+                try {
+                    stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+
+    //  Everything below this line is within steeringButtonDepressed
+
+    public void forward() throws InterruptedException {
+
+        GpioPinDigitalOutput wheelOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26);
+        GpioPinDigitalOutput wheelTwo = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
+        GpioPinDigitalOutput wheelThree = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
+        GpioPinDigitalOutput wheelFour = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
+
+        wheelOne.high();
+        wheelTwo.high();
+        wheelThree.high();
+        wheelFour.high();
+    }
+
+    public void stop() throws InterruptedException {
+
+        GpioPinDigitalOutput wheelOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26);
+        GpioPinDigitalOutput wheelTwo = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
+        GpioPinDigitalOutput wheelThree = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
+        GpioPinDigitalOutput wheelFour = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
+
+        wheelOne.high();
+        wheelOne.toggle();
+        wheelTwo.high();
+        wheelTwo.toggle();
+        wheelThree.high();
+        wheelThree.toggle();
+        wheelFour.high();
+        wheelFour.toggle();
+    }
+
+    public void left() throws InterruptedException {
+
+        GpioPinDigitalOutput wheelOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26);
+        GpioPinDigitalOutput wheelTwo = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
+        GpioPinDigitalOutput wheelThree = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
+        GpioPinDigitalOutput wheelFour = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
+
+        wheelOne.high();
+        wheelOne.toggle();
+        wheelTwo.high();
+        wheelTwo.toggle();
+        wheelThree.high();
+        wheelFour.high();
+    }
+
+    public void right() throws InterruptedException {
+
+        GpioPinDigitalOutput wheelOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26);
+        GpioPinDigitalOutput wheelTwo = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
+        GpioPinDigitalOutput wheelThree = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
+        GpioPinDigitalOutput wheelFour = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
+
+        wheelOne.high();
+        wheelTwo.high();
+        wheelThree.high();
+        wheelThree.toggle();
+        wheelFour.high();
+        wheelFour.toggle();
+    }
+//  Everything above this line is within steeringButtonDepressed
+
+//  Everything below this line is within steeringButtonReleased
+
+    public void forwardRelease() throws InterruptedException {
+
+        GpioPinDigitalOutput wheelOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26);
+        GpioPinDigitalOutput wheelTwo = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
+        GpioPinDigitalOutput wheelThree = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
+        GpioPinDigitalOutput wheelFour = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
+
+        wheelOne.low();
+        wheelTwo.low();
+        wheelThree.low();
+        wheelFour.low();
+    }
+
+    public void rightRelease() throws InterruptedException {
+
+        GpioPinDigitalOutput wheelOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26);
+        GpioPinDigitalOutput wheelTwo = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
+        GpioPinDigitalOutput wheelThree = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
+        GpioPinDigitalOutput wheelFour = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
+
+        wheelOne.low();
+        wheelTwo.low();
+        wheelThree.low();
+        wheelFour.low();
+    }
+
+    public void leftRelease() throws InterruptedException {
+
+        GpioPinDigitalOutput wheelOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26);
+        GpioPinDigitalOutput wheelTwo = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
+        GpioPinDigitalOutput wheelThree = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
+        GpioPinDigitalOutput wheelFour = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
+
+        wheelOne.high();
+        wheelOne.toggle();
+        wheelTwo.high();
+        wheelTwo.toggle();
+        wheelThree.high();
+        wheelFour.high();
+    }
+
+
+}
